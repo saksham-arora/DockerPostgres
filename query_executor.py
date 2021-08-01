b000df5e71799d60314258f866efe8d4ef6559cd
Saksham <saksham.arora501@gmail.com>
2021-08-01 23:35:08 +0530

    code update as per #PR1

--- query_executor.py
+++ query_executor.py
@@ -1,18 +1,20 @@
 import csv
+import sys
+import pytest
 
-with open('E:\querycsv-redux-4.1.0.tar\dist\querycsv-redux-4.1.0\querycsv\\dim_vendors.csv') as csv_file:
-    csv_reader_dim_vendors = csv.DictReader(csv_file, delimiter=',')
+with open(sys.argv[1]) as csv_file_vendors:
+    csv_reader_dim_vendors = csv.DictReader(csv_file_vendors, delimiter=',')
     vendors_map={}
     for row_vendors in csv_reader_dim_vendors:
         vendors_map[row_vendors["id"]]=row_vendors["type"]
 
-with open('E:\querycsv-redux-4.1.0.tar\dist\querycsv-redux-4.1.0\querycsv\\fact_orders.csv') as csv_file:
-    csv_reader_fact_orders = csv.DictReader(csv_file, delimiter=',')
+with open(sys.argv[2]) as csv_file_orders:
+    csv_reader_fact_orders = csv.DictReader(csv_file_orders, delimiter=',')
     result_map={}
     for row_orders in csv_reader_fact_orders:
         for vendor in vendors_map.keys():
             if vendor == row_orders["vendor_id"]:
-                if row_orders["status"]=='2' or row_orders["status"]=="3":
+                if row_orders["status"] in ['2','3']:
                     if vendor in result_map.keys():
                         result_map[vendor]+=float(row_orders["amt"])
                     else:
@@ -20,4 +22,6 @@ with open('E:\querycsv-redux-4.1.0.tar\dist\querycsv-redux-4.1.0\querycsv\\fact_
     result_map=dict(sorted(result_map.items(), key=lambda item: -item[1]))
     for row in result_map.keys():
         print(row + " " + vendors_map[row] + " " + str(result_map[row]))
-    print(result_map)
+        
+def test_file_check():
+    assert csv_file_vendors and csv_file_orders
